f1906fbdef20b8c339678b48eb4a4ebf1ea1981d
tbs03171 <tbs03171@naver.com>
2023-01-11 15:41:12 +0900

    first commit

---
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 화면 출력 XML 명시
+        setContentView(R.layout.activity_main)
+    }
+}
