c53042aca0768502e64c87cf0c36b4e648806ad1
ParkHyunJung <tbs03171@naver.com>
2023-02-01 11:50:23 +0900

    20230201

---
+++ ch10_notification/src/main/java/com/example/ch10_notification/ReplyReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.ch10_notification
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // 알림의 입력 글 획득
+        val replyTxt = androidx.core.app.RemoteInput.getResultsFromIntent(intent)
+            ?.getCharSequence("key_text_reply")
+        Log.d("kkang", "replyTxt : $replyTxt")
+        // 알림 취소
+        val manager = context.getSystemService(
+            AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
+    }
+}
